52d783728f9c55e8b79c5f404c5b723f276c30bb
Cupcakes33 <rute930627@gmail.com>
2022-12-16 20:47:55 +0900

    :tada: chore: 1차 파일디렉토링

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {/* <img src="img/naobabLogo.png" style={{ width: "200px" }}></img>
+      <img src="img/favicon.ico" style={{ width: "32px" }}></img>
+      <img src="img/weather.png" style={{ width: "100px" }}></img> */}
     </div>
   );
 }
