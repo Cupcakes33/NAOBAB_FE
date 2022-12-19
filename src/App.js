acf58fd191f03b09a4cbaa179fce55fae036216d
Cupcakes33 <rute930627@gmail.com>
2022-12-19 15:58:41 +0900

    1차 배포

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
 import React from "react";
+import Main from "./mainpage/Main";
+import Postpage from "./postpage/Postpage";
+import LoginPage from "./loginpage/LoginPage";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/mainpage" element={<Main />} />
+      <Route path="/postpage" element={<Postpage />} />
+      <Route path="/detailpage/:postId" element={<Postpage />} />
+      {/* 수정버튼을 누르면  */}
+    </Routes>
+  );
 };
-
 
 export default App;
