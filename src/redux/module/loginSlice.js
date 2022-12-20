dbe7ef86ae19e30b58d66c8ff04153ad4a74dd09
정창원 <8500jcw@gmail.com>
2022-12-20 15:41:28 +0900

    토큰있을시 로그인->메인페이지 이동 구현

--- src/redux/module/loginSlice.js
+++ src/redux/module/loginSlice.js
@@ -76,7 +76,7 @@ const loginSlice = createSlice({
       state.loading = false;
       // state.login = [...state.login, action.payload];
       alert("로그인이 확인되었습니다!");
-      // window.location.replace("http://localhost:3000/mainpage");
+      window.location.replace("http://localhost:3001/mainpage");
     },
     [signInUser.rejected]: (state, action) => {
       state.loading = false;
